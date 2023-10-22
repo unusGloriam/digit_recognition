9681dfbaaa7983fd0f9f84301413bc53b3e80bc2
unusGloriam <televisor2x2@gmail.com>
2023-10-22 17:22:31 +0300

    Теперь адекватный персептрон

--- perceptron.cpp
+++ perceptron.cpp
@@ -1,4 +1,5 @@
 #include "perceptron.h"
+#include "qdebug.h"
 
 Perceptron::Perceptron(Matrix in) {
     input_matrix = in;
@@ -52,24 +53,65 @@ QVector<int> Perceptron::get_sum_scaled_signals() {
 int Perceptron::recognition() {
     int index_neyron = 0;
     int max_sum_scaled_signals = neurons[index_neyron].count_sum_scaled_signals(input_matrix);
-
+    qDebug() << "0 sum: " << max_sum_scaled_signals;
     for (int i = 1; i < NUM_DIGITS; ++i) {
         int sum_scaled_signals = neurons[i].count_sum_scaled_signals(input_matrix);
+        qDebug() << i << " sum: " << sum_scaled_signals;
         if (sum_scaled_signals > max_sum_scaled_signals) {
             max_sum_scaled_signals = sum_scaled_signals;
             index_neyron = i;
         }
     }
+//    for (int i = 0; i < NUM_DIGITS; ++i){
+//        neurons[i].count_sum_scaled_signals(input_matrix);
+//        if (neurons[i].recognition(input_matrix)){
+//            index_neyron = i;
+//            break;
+//        }
+//    }
     return index_neyron;
 }
 
 QVector<Matrix> Perceptron::teach(int correct_index) {
-    for (int i = 0; i < NUM_DIGITS; ++i) {
-        if (i != correct_index && neurons[i].recognition()) {
+//    int max_sum_scaled_signals = neurons[0].count_sum_scaled_signals(input_matrix);
+//    int max_counter = 1;
+//    int max_index = 0;
+    // сначала нейроны
+    for (int i = 0; i < NUM_DIGITS; ++i){
+        //neurons[i].count_sum_scaled_signals(input_matrix);
+        if (neurons[i].recognition(input_matrix) && i != correct_index){
             neurons[i].dec_weight(input_matrix);
+        } else if (i == correct_index && !neurons[i].recognition(input_matrix)){
+            neurons[i].inc_weight(input_matrix);
         }
     }
 
-    neurons[correct_index].inc_weight(input_matrix);
+    // теперь максимумы
+//    for (int i = 0; i < NUM_DIGITS; ++i){ // ищем наибольшую сумму
+//        int current_sum = neurons[i].count_sum_scaled_signals(input_matrix);
+//        if (current_sum > max_sum_scaled_signals){
+//            max_sum_scaled_signals = current_sum;
+//            max_counter = 1;
+//            max_index = i;
+//        } else if (current_sum == max_sum_scaled_signals){
+//            ++max_counter;
+//        }
+//    }
+//    if (max_counter > 1){ // если сразу несколько максимумов
+//        for (int i = 0; i < NUM_DIGITS; ++i){
+//            int current_sum = neurons[i].count_sum_scaled_signals(input_matrix);
+//            if (i == correct_index){
+//                neurons[correct_index].inc_weight(input_matrix);
+//            } else if (current_sum == max_sum_scaled_signals){
+//                neurons[i].dec_weight(input_matrix);
+//            }
+//        }
+//    } else { // если один максимум...
+//        if (max_index != correct_index){ // ...и он неправильный
+//            neurons[max_index].dec_weight(input_matrix);
+//            neurons[correct_index].inc_weight(input_matrix);
+//        }
+//    }
+
     return get_weight_matrix();
 }
